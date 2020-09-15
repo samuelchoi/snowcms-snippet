5174a8d7e9a310b320d2a9c386ac53cc78f3f9b0
samuelchoi <samuelchoi@163.com>
2020-09-15 18:37:44 +0800

    modif log system x3

--- src/main/java/org/snow/snippet/log/Log.java
+++ src/main/java/org/snow/snippet/log/Log.java
@@ -2,36 +2,42 @@ package org.snow.snippet.log;
 
 public interface Log {
 
+    /* fatal */
     boolean isFatalEnabled();
 
     void fatal(Object message);
 
     void fatal(Object message, Throwable t);
 
+    /* error */
     boolean isErrorEnabled();
 
     void error(Object message);
 
     void error(Object message, Throwable t);
 
+    /* warn */
     boolean isWarnEnabled();
 
     void warn(Object message);
 
     void warn(Object message, Throwable t);
 
+    /* info */
     boolean isInfoEnabled();
 
     void info(Object message);
 
     void info(Object message, Throwable t);
 
+    /* debug */
     boolean isDebugEnabled();
 
     void debug(Object message);
 
     void debug(Object message, Throwable t);
 
+    /* trace */
     boolean isTraceEnabled();
 
     void trace(Object message);
